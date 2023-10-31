e217b17c5e2daddd73db02d5ba811344a3dfbb57
Adam Metz <alm939@uregina.ca>
2023-10-30 19:42:45 -0600

    Improve namings, fix purchasing bug

--- src/scenes/Main.gd
+++ src/scenes/Main.gd
@@ -9,27 +9,27 @@ func _ready():
 	for generator in $GeneratorsPanelBackground/GeneratorsContainer.get_children():
 		if generator is BaseGenerator:
 			generator.generator_purchased.connect(on_generator_purchased)
-	updatePoints(0)
+	update_points(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func updatePoints(pointsDelta : int):
+func update_points(pointsDelta : int):
 	if (sustainability_points + pointsDelta >= 0):
 		sustainability_points += pointsDelta
 		$PointDisplay.text = str(sustainability_points)
 
-func updatePointsPerSecond(points_per_second_delta: int):
+func update_points_per_second(points_per_second_delta: int):
 	points_per_second += points_per_second_delta
 
 func _on_clicker_button_button_down():
-	updatePoints(points_per_click)
-
+	update_points(points_per_click)
 
 func _on_timer_timeout():
-	updatePoints(points_per_second)
+	update_points(points_per_second)
 
 func on_generator_purchased(generator : BaseGenerator):
-	points_per_second += generator.get_points_per_second()
+	update_points_per_second(generator.get_points_per_second())
+	update_points(-generator.get_cost())
