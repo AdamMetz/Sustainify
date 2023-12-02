e6fc7f1a48ac0ed4daeba95bebe66de8ff2a23c1
Adam Metz <alm939@uregina.ca>
2023-12-02 12:38:30 -0600

    Add rudimentary popup functionality and assets

--- src/scenes/EducationalPopup.gd
+++ src/scenes/EducationalPopup.gd
@@ -1,31 +1,12 @@
-extends Popup
+extends TextureRect
 
-class_name EducationalPopup
+var allow_continue = false
 
-var title_label : Label
-var text_label : Label
-var background_texture : TextureRect
+func _on_gui_input(event):
+	if (allow_continue and event is InputEventMouseButton):
+		if (event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+			allow_continue = false
+			self.hide()
 
-func init(title : String, text : String):
-	title_label.text = title
-	text_label.text = text
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	title_label = Label.new()
-	text_label = Label.new()
-	background_texture = TextureRect.new()
-
-	title_label.text = "Test"
-	title_label.modulate = Color(0, 0, 0)
-	text_label.text = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
-	#text_label.autowrap_mode = TextServer.AUTOWRAP_WORD
-	text_label.modulate = Color(0, 0, 0)
-
-	add_child(background_texture)
-	add_child(title_label)
-	add_child(text_label)
-
-	background_texture.texture = preload("res://assets/UI-Background.png")
-
-	popup_centered()
+func _on_timer_timeout():
+	allow_continue = true
