4ef9d23cfc39d65ea40293aa655bea549a5e05e6
Adam Metz <alm939@uregina.ca>
2023-12-04 22:12:22 -0600

    Add educational popup source links, and recurring popups

--- src/scenes/EducationalPopup.gd
+++ src/scenes/EducationalPopup.gd
@@ -3,11 +3,13 @@ extends TextureRect
 var allow_continue = false
 var title_node
 var text_node
+var source_link_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	title_node = get_node("EducationalPopupPanel/VBoxContainer/TitleLabel")
 	text_node = get_node("EducationalPopupPanel/VBoxContainer/TextLabel")
+	source_link_node = get_node("EducationalPopupPanel/SourceLinkButton")
 
 func _on_gui_input(event):
 	if (allow_continue and event is InputEventMouseButton):
@@ -23,3 +25,6 @@ func set_title(title: String):
 
 func set_text(text: String):
 	text_node.text = text
+	
+func set_source_link(uri: String):
+	source_link_node.uri = uri
--- src/scenes/InfoAlert.gd
+++ src/scenes/InfoAlert.gd
@@ -6,6 +6,7 @@ var moving_up = true
 var main_node
 var popup_node
 var popup_timer_node
+var unlocked = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,16 +29,40 @@ func _process(delta):
 		else: moving_up = true	
 
 func _on_gui_input(event):
+	# TODO: Refactor code to better generate popups, rather than a large amount of if statements
 	if (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
-		self.hide()
-		if (self.name == "WindTurbineInfoAlert"):
-			popup_node.set_title("Wind Turbine")
-			popup_node.set_text("Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ")
-		elif (self.name == "SolarPanelInfoAlert"):
-			popup_node.set_title("Solar Panel")
-			popup_node.set_text("Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ")
-		elif (self.name == "ReforestationInfoAlert"):
-			popup_node.set_title("Reforestation")
-			popup_node.set_text("Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ")
+		if !unlocked:
+			self.unlocked = true
+			if (self.name == "WindTurbineInfoAlert"):
+				popup_node.set_title("Wind Turbine")
+				popup_node.set_text("Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ")
+				popup_node.set_source_link("www.google.ca")
+			elif (self.name == "SolarPanelInfoAlert"):
+				popup_node.set_title("Solar Panel")
+				popup_node.set_text("Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ")
+				popup_node.set_source_link("www.google.ca")
+			elif (self.name == "ReforestationInfoAlert"):
+				popup_node.set_title("Reforestation")
+				popup_node.set_text("Reforestation is like planting a lot of new trees in places where they were cut down or where they didn't grow before. It's like giving the Earth more green, leafy friends.")
+				popup_node.set_source_link("www.google.ca")
+		else:
+			if (self.name == "WindTurbineInfoAlert"):
+				popup_node.set_title("Wind Turbine")
+				popup_node.set_text("Wind")
+				popup_node.set_source_link("www.google.ca")
+			elif (self.name == "SolarPanelInfoAlert"):
+				popup_node.set_title("Solar Panel")
+				popup_node.set_text("Solar")
+				popup_node.set_source_link("www.google.ca")
+			elif (self.name == "ReforestationInfoAlert"):
+				popup_node.set_title("Reforestation")
+				popup_node.set_text("Refor")
+				popup_node.set_source_link("www.google.ca")
 		popup_timer_node.start(1)
 		popup_node.show()
+		self.hide()
+
+
+func _on_timer_timeout():
+	if self.unlocked:
+		self.show()
